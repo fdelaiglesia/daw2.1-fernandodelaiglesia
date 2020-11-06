60c95f3765b6362795d554f4a5dc4cd1c06e1cea
Fernando <fernando.iglesia1@educa.madrid.org>
2020-11-06 12:36:10 +0100

    Deberes: 1)Añadido apellido, 2) añadido favorito, 3) añadido solo favoritos, añadida estrella

--- 002-agenda/persona-listado.php
+++ 002-agenda/persona-listado.php
@@ -61,7 +61,7 @@ $personas = $select->fetchAll();
                                   d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                         </svg>
                     </a></td>
-                <td><?php if ($filaUnica["p_estrella"] == 1) {
+                <td><?php if ($filaUnica["p_estrella"]) {
                         echo '<img src="estrella.png" width="25" height="25">';
                     } ?>
 
